3be941b4a35415f1305c8d81d734642b36607c1e
Vit <mayllby@gmail.com>
2024-01-28 01:24:12 +0300

    Добавил оператор 'pass'

--- home_add.py
+++ home_add.py
@@ -33,3 +33,19 @@ if number == 5:
     # continue # VSCode не позволяет воспользоваться оператором "continue"
 print('Number is ' + str(number))
 print('Out of loop')
+
+
+'''
+Оператор pass
+
+При возникновении внешнего фактора оператор pass 
+устраняет любое влияние этого фактора на обработку кода. 
+Код обрабатывается до тех пор, пока не появится break или другой оператор.
+'''
+number = 0
+for number in range(10):
+    number = number + 1
+if number == 5:
+    pass
+print('Number is ' + str(number))
+print('Out of loop')
