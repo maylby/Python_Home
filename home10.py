88e8a939ce7dcb01442bd25add41f6d1cb807565
Vit <mayllby@gmail.com>
2024-02-05 20:41:34 +0300

    Добавил условие задачи

--- home10.py
+++ home10.py
@@ -1,3 +1,19 @@
 # Знакомство с языком Python (семинары)
 # Урок 10. Построение графиков
 # https://gb.ru/lessons/391161/homework
+
+"""
+Задача 44: 
+
+В ячейке ниже представлен код генерирующий DataFrame, 
+которая состоит всего из 1 столбца. 
+Ваша задача перевести его в one hot вид. 
+Сможете ли вы это сделать без get_dummies?
+"""
+
+import random
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI':lst})
+data.head()
