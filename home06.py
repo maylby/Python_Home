89596b5c7c5ba82557a582feb5d305d3d70d9a4f
Vit <mayllby@gmail.com>
2024-02-08 19:02:59 +0300

    Добавил решение задачи

--- home06.py
+++ home06.py
@@ -33,6 +33,29 @@
 # 16
 # 19
 
+"""
+Решение
+
+Вариант 1
+"""
+#	         1                          9            13  14                19
+list1 = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
+#	      0  1  2  3   4   5  6  7   8  9  10  11 12  13 14  15  16 17  18  19
+
+min = int(input('min: '))
+max = int(input('max: '))
+
+def search(arr): # 'arr' - пустой список для передачи значений 'list1'
+	result = []
+	for i in range(len(arr)): 
+		if min <= list1[i] <= max: # допустимая форма записи
+			result.append(i) # добавление индексов элементов
+	return result 
+
+print('Ввод: ', list1)
+print('Вывод:', search(list1)) 
+
+
 
 
 """
