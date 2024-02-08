fa36b74f865cb300f29d34b2e8034c19f83e7701
Vit <mayllby@gmail.com>
2024-02-08 21:57:27 +0300

    Добавил решение

--- home09.py
+++ home09.py
@@ -26,10 +26,18 @@
 где количество людей от 0 до 500 (population) и сохранить ее в переменную avg. 
 Используйте модуль pandas.
  
-Без библиотек "pandas" в VSCode работать не будет
+Без библиотек "pandas", код в VSCode работать не будет.
 
-Решение для автотеста,
+
+Решение (автотест)
 """
+
+import pandas as pd # Сообщение VSCode: "не удалось импортировать «pandas»"
+df = pd.read_csv('california_housing_train.csv')
+
+avg = df[(df['population'] >= 0) & (df['population'] <= 500)] ['median_house_value'].mean()
+print(avg) 
+
 
 '''
 Запрос по 'head(10)'
